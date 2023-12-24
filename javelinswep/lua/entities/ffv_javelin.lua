651cf9e7c7599fd5f2c87bc03857ffda5f066dbb
wthan1 <ethan33t@gmail.com>
2023-12-24 17:57:57 -0600

    javelin swep

--- javelinswep/lua/entities/ffv_javelin.lua
+++ javelinswep/lua/entities/ffv_javelin.lua
@@ -2,6 +2,10 @@ AddCSLuaFile()
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
+ENT.awaitingWeld = false
+ENT.object = nil
+ENT.objectBone = 0
+
 function ENT:Initialize()
 	if CLIENT then return end
 	self:SetModel("models/props_junk/harpoon002a.mdl")
@@ -12,10 +16,17 @@ function ENT:Initialize()
 end
 
 function ENT:PhysicsCollide(colData, collider)
+	if CLIENT then return end
 	local ent = colData.HitEntity
-	if (not (ent == game.GetWorld())) then return end
-	if ((colData.Speed > 1000) and ((self:GetAngles():Forward() - colData.HitNormal):Length() < 1)) then
-		--this is so it makes the hole in the wall with the right sound n stuff
+	local speed = colData.OurOldVelocity:Length()
+	if (not (ent == game.GetWorld())) then
+		if (speed > 2000) then ent:TakeDamage(speed/20,self:GetNWEntity("thrower"),self) end
+		if (ent:IsNPC() or ent:IsPlayer()) then return end
+	end
+	if (colData.TheirSurfaceProps == 76) then return end
+
+	if ((speed > 1000) and ((self:GetAngles():Forward() - colData.HitNormal):Length() < 1)) then
+		--this was the best way i could think of to get the hole be the right material and sound
 		local bullet = {}
 		bullet.Damage = 0
 		bullet.Src = self:GetPos()
@@ -23,6 +34,31 @@ function ENT:PhysicsCollide(colData, collider)
 		self:FireBullets(bullet)
 
 		self:GetPhysicsObject():EnableMotion(false)
+		if IsValid(ent:GetPhysicsObject()) then ent:GetPhysicsObject():EnableMotion(false) end
+		self.awaitingWeld = true
+		self.object = ent
+		self.objectBone = util.QuickTrace(self:GetPos(),self:GetAngles():Forward()*999,self).PhysicsBone
+	end
+end
+
+function ENT:Think()
+	if CLIENT then return end
+	if self.awaitingWeld then
+		self:SetPos(self:GetPos()+(self:GetAngles():Forward()*10))
+
+		local weldPart = game.GetWorld()
+		if IsValid(self.object) then
+			weldPart = self.object
+			weldPart:GetPhysicsObject():EnableMotion(true)
+			self:GetPhysicsObject():EnableMotion(true)
+
+		elseif (not (self.object == game.GetWorld())) then
+			--it hit an object but its gone by the time we get to here
+			weldPart = nil
+			self:GetPhysicsObject():EnableMotion(true)
+		end
+		constraint.Weld(self,weldPart,0,self.objectBone,10000,true,false)
+		self.awaitingWeld = false
 	end
 end
 
