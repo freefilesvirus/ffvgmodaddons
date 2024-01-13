58f691a804591655556defb904115ab1382b4588
wthan1 <ethan33t@gmail.com>
2024-01-13 14:56:42 -0600

    fml
    i put together the addon in ffvjunk cause i wasnt sure if i would actually end up uploading it or not and forgot to add this script which he needs to MOVE... at least i solved it fast but that really sucks so much man

---
+++ ffvrobots/lua/wiki_astar.lua
@@ -0,0 +1,79 @@
+-- code "borrowed" from gmod wiki... thanks gmod lua wiki writers
+function Astar( start, goal )
+	if ( !IsValid( start ) || !IsValid( goal ) ) then return false end
+	if ( start == goal ) then return true end
+
+	start:ClearSearchLists()
+
+	start:AddToOpenList()
+
+	local cameFrom = {}
+
+	start:SetCostSoFar( 0 )
+
+	start:SetTotalCost( heuristic_cost_estimate( start, goal ) )
+	start:UpdateOnOpenList()
+
+	while ( !start:IsOpenListEmpty() ) do
+		local current = start:PopOpenList() -- Remove the area with lowest cost in the open list and return it
+		if ( current == goal ) then
+			return reconstruct_path( cameFrom, current )
+		end
+
+		current:AddToClosedList()
+
+		for k, neighbor in pairs( current:GetAdjacentAreas() ) do
+			local newCostSoFar = current:GetCostSoFar() + heuristic_cost_estimate( current, neighbor )
+
+			if ( neighbor:IsUnderwater() ) then -- Add your own area filters or whatever here
+				continue
+			end
+			
+			if ( ( neighbor:IsOpen() || neighbor:IsClosed() ) && neighbor:GetCostSoFar() <= newCostSoFar ) then
+				continue
+			else
+				neighbor:SetCostSoFar( newCostSoFar );
+				neighbor:SetTotalCost( newCostSoFar + heuristic_cost_estimate( neighbor, goal ) )
+
+				if ( neighbor:IsClosed() ) then
+				
+					neighbor:RemoveFromClosedList()
+				end
+
+				if ( neighbor:IsOpen() ) then
+					-- This area is already on the open list, update its position in the list to keep costs sorted
+					neighbor:UpdateOnOpenList()
+				else
+					neighbor:AddToOpenList()
+				end
+
+				cameFrom[ neighbor:GetID() ] = current:GetID()
+			end
+		end
+	end
+
+	return false
+end
+
+function heuristic_cost_estimate( start, goal )
+	-- Perhaps play with some calculations on which corner is closest/farthest or whatever
+	return start:GetCenter():Distance( goal:GetCenter() )
+end
+
+-- using CNavAreas as table keys doesn't work, we use IDs
+function reconstruct_path( cameFrom, current )
+	local total_path = { current }
+
+	current = current:GetID()
+	while ( cameFrom[ current ] ) do
+		current = cameFrom[ current ]
+		table.insert( total_path, navmesh.GetNavAreaByID( current ) )
+	end
+	return total_path
+end
+
+function AstarVector( start, goal )
+	local startArea = navmesh.GetNearestNavArea( start )
+	local goalArea = navmesh.GetNearestNavArea( goal )
+	return Astar( startArea, goalArea )
+end
