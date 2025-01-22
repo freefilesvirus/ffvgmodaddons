6e0dc6720ee23b03f05dd906dd0d69b890b345d9
wthan1 <113853733+wthan1@users.noreply.github.com>
2025-01-22 08:26:44 -0600

    hall of mirrooooooooorrrrrrr

--- hommat/lua/autorun/ffv_hommat.lua
+++ hommat/lua/autorun/ffv_hommat.lua
@@ -1,33 +1,33 @@
-list.Add("OverrideMaterials","!ffvHallOfMirrors")
-if SERVER then return end
+if SERVER then
+	util.AddNetworkString("ffvHOMTable")
+
+	return
+end
+
+local ffvHOMRelevant={}
+
+hook.Add("InitPostEntity","ffvHOMSpawn",function(ply)
+	if SERVER then return end
+
+	for _,e in ents.Iterator() do
+		if e:GetNWBool("ffv_homed") then table.insert(ffvHOMRelevant,e) end
+	end
+end)
+
+net.Receive("ffvHOMTable",function() ffvHOMRelevant=net.ReadTable() end)
 
 local texture=GetRenderTarget(
 	"ffvGlitchTexture",
 	ScrW(),ScrH()
 )
-CreateMaterial(
-	"ffvHallOfMirrors",
-	"UnlitGeneric",
-	{["$basetexture"]="ffvGlitchTexture"}
-)
 
 hook.Add("RenderScene","ffvGlitchRender",function()
-	render.RenderView()
+	render.RenderView({})
 	render.CopyRenderTargetToTexture(texture)
 end)
 
 hook.Add("PostDrawOpaqueRenderables","ffvGlitchDraw",function()
-	local relevant={}
-	for _,e in ents.Iterator() do
-		local isRelevant=(e:GetMaterial()=="!ffvHallOfMirrors")
-		local k=1
-		while ((not isRelevant) and (k<=#e:GetMaterials())) do
-			if (e:GetSubMaterial(k)=="!ffvHallOfMirrors") then isRelevant=true end
-			k=(k+1)
-		end
-		if isRelevant then table.insert(relevant,e) end
-	end
-	if (#relevant==0) then return end
+	if (#ffvHOMRelevant==0) then return end
 
 	render.ClearStencil()
 	render.SetStencilWriteMask( 255 )
@@ -41,8 +41,8 @@ hook.Add("PostDrawOpaqueRenderables","ffvGlitchDraw",function()
 	render.SetStencilCompareFunction( STENCILCOMPARISONFUNCTION_ALWAYS )
 	render.SetStencilPassOperation( STENCILOPERATION_REPLACE )
 
-	for _,e in pairs(relevant) do
-		if (halo.RenderedEntity()~=e) then e:DrawModel() end
+	for _,e in pairs(ffvHOMRelevant) do
+		if (IsValid(e) and (halo.RenderedEntity()~=e)) then e:DrawModel() end
 	end
 
 	render.SetStencilFailOperation( STENCILOPERATION_KEEP )
---
+++ hommat/lua/weapons/gmod_tool/stools/ffv_homapplier.lua
@@ -0,0 +1,45 @@
+TOOL.Category="Render"
+TOOL.Name="#tool.ffv_homapplier.name"
+
+if CLIENT then
+	TOOL.Information={
+		{name="left"},
+		{name="right"}
+	}
+
+	language.Add("tool.ffv_homapplier.name","hall of mirrorizer")
+	language.Add("tool.ffv_homapplier.desc","apply the hall of mirrors glitch effect")
+	language.Add("tool.ffv_homapplier.left","apply")
+	language.Add("tool.ffv_homapplier.right","clear")
+end
+
+function TOOL:LeftClick(trace)
+	local good=(IsValid(trace.Entity) and (trace.Entity~=game.GetWorld()) and (not trace.Entity:GetNWBool("ffv_homed")))
+	
+	trace.Entity:SetNWBool("ffv_homed",true)
+
+	self:networkUpdatedTable()
+	return good
+end
+
+function TOOL:RightClick(trace)
+	local good=(IsValid(trace.Entity) and trace.Entity:GetNWBool("ffv_homed"))
+	
+	trace.Entity:SetNWBool("ffv_homed",false)
+
+	self:networkUpdatedTable()
+	return good
+end
+
+function TOOL:networkUpdatedTable()
+	if CLIENT then return end
+	
+	local relevant={}
+	for _,e in ents.Iterator() do
+		if e:GetNWBool("ffv_homed") then table.insert(relevant,e) end
+	end
+
+	net.Start("ffvHOMTable")
+		net.WriteTable(relevant)
+	net.Broadcast()
+end
